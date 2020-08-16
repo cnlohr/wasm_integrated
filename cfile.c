da85b10138802eac7d8bb99a4d056f8b8c66fe7f
CNLohr <lohr85@gmail.com>
2020-08-15 21:50:25 -0400

    bump - with graphics

--- cfile.c
+++ cfile.c
@@ -1,8 +1,7 @@
 #include <emscripten.h>
 #include <stdio.h>
 #include <unistd.h>
-
-//int __stack_pointer = 0;
+#include <math.h>
 
 int add2( int a, int b )
 {
@@ -12,7 +11,10 @@ int add2( int a, int b )
 //Function in Javascript Land
 extern void writeout( int o );
 extern void writeoutStr( const char * o );
-
+extern void beginPath();
+extern void stroke();
+extern void tackSegment( int x1, int y1, int x2, int y2 );
+extern void canvasClear();
 
 int testcallback( int z )
 {
@@ -24,3 +26,27 @@ int testcallback( int z )
 
 	return z+10000;
 }
+
+int frame;
+
+void animationFrame()
+{
+	int i;
+	frame++;
+	canvasClear();
+	beginPath();
+
+	for( i = 0; i <= 62; i++ )
+	{
+		int r1 = 120;
+		int r2 = 120;
+		int cx = 320;
+		int cy = 240;
+		float a1 = i / 10.    + frame/60.;
+		float a2 = i / 10. + 1;
+		tackSegment( cx+cosf(a1)*r1, cy+sinf(a1)*r1, cx+cosf(a2)*r2, cy+sinf(a2)*r2 );
+	}
+	stroke();
+}
+
+
